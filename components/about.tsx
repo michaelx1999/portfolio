55ee094018d2e25a33f404e556ed64e0af94ae42
michael <michaelxia29@gmail.com>
2024-04-04 19:20:21 -0400

    update about + skills

--- components/about.tsx
+++ components/about.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <section className="w-1/2 space-y-5 scroll-mt-28" id="about">
       <h2 className="font-medium text-3xl text-gray-700 text-center">About Me</h2>
-      <p className="font-mdium text-lg text-gray-700 bg-gray-100 rounded-xl p-5 hover:scale-110 transition">
+      <p className="font-mdium text-lg text-gray-700 rounded-xl p-5 hover:scale-110 transition">
         I have a deep passion for solving complex problems through innovative software solutions. My
         expertise spans a variety of programming languages and development frameworks, enabling me
         to create efficient, robust, and scalable applications. I excel in collaborative
--- components/skills.tsx
+++ components/skills.tsx
@@ -4,7 +4,7 @@ export default function Skills() {
   return (
     <section className="w-1/2 space-y-5 scroll-mt-28" id="skills">
       <h2 className="font-medium text-3xl text-gray-700 text-center">My Skills</h2>
-      <ul className="flex flex-wrap gap-5 text-lg text-gray-800 justify-center bg-gray-100 rounded-xl p-5 hover:scale-110 transition">
+      <ul className="flex flex-wrap gap-5 text-lg text-gray-800 justify-center rounded-xl p-5 hover:scale-110 transition">
         {skills.map((skill, index) => (
           <li className=" bg-white p-2 border rounded-xl" key={index}>
             {skill}
