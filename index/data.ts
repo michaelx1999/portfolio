7d8bda3d88e541e393eb0edb45d1dbdcfba2531d
michael <michaelxia29@gmail.com>
2024-04-04 22:41:05 -0400

    update data

--- index/data.ts
+++ index/data.ts
@@ -36,6 +36,7 @@ export interface ProjectData {
   title: string
   description: string
   tags: string[]
+  link: string
   imageUrl: StaticImageData
 }
 
@@ -43,14 +44,16 @@ export const projects: ProjectData[] = [
   {
     title: "CICD Pipeline",
     description:
-      "Demonstrates a complete backend workflow, from testing, to building, then to deployment",
+      "Developed a CRUD application with FastAPI and PostgreSQL, ensuring its reliability with 100% test coverage and enhancing deployment efficiency through automated CI/CD pipelines using GitHub Actions, AWS ECR, and Docker.",
     tags: ["Python", "FastAPI", "Testing", "AWS", "GitHub Action"],
+    link: "https://github.com/michaelx1999/cicd",
     imageUrl: cicd
   },
   {
     title: "Portfolio",
-    description: "My portfolio page, demonstrates my design skill",
+    description: "Developed a personal portfolio website using Next.js for server-side rendering and TypeScript for type-checking, prioritizing performance, user experience, and code maintainability.",
     tags: ["HTML", "Tailwind CSS", "React", "Next Js"],
+    link: "https://github.com/michaelx1999/portfolio",
     imageUrl: portfolio
   }
 ] as const
@@ -68,16 +71,16 @@ export const experience: ExperienceData[] = [
   {
     title: "Software Engineer Intern",
     company: "Symetra",
-    location:"Bellevue, WA",
-    description: "Develop and maintain backend microservices & cloud infrastructure",
+    location: "Bellevue, WA",
+    description: "Develop and maintain backend microservices & cloud infrastructure.",
     icon: React.createElement(CgWorkAlt),
     date: "May 2023 - Aug 2023"
   },
   {
     title: "Software Engineer Intern",
     company: "NASA NYSG",
-    location:"Owego, NY",
-    description: "Sensor detection development using Arduino Uno",
+    location: "Owego, NY",
+    description: "Sensor detection development using Arduino Uno.",
     icon: React.createElement(CgWorkAlt),
     date: "Jun 2022 - Aug 2022"
   }
